d4c72e0de621945090e31479335c360ac39c1d4a
firas khurshid <firas@saibbyweb.com>
2026-09-17 15:42:54 +0530

    javascript submit form

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function greetUser(){
-    return "What's on your mind today!?";
-} 
-document.getElementById("ndtv").innerHTML = greetUser();
+console.log("Form Details:\n ");
+const form = document.querySelector(".forum");
+form.addEventListener("submit",function(event) { event.preventDefault();
+const inputs = document.querySelectorAll(".inn");
+inputs.forEach(function(input){
+    
+    console.log(input.id + ":" , input.value)
+});
+});
+
